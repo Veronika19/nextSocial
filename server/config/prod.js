59c48db62abf05b7ef8a2c28aade2305b9dff823
Veronika Sahi <veronicaasahii@gmail.com>
2020-06-26 11:57:25 +0530

    added keys to prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,11 +1,26 @@
 module.exports = {
-	host: process.env.DB_HOST,
-	database: process.env.DB_DATABASE,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	secretOrKey: process.env.DB_SecretOrKey,
-	googleClientID: process.env.GOOGLE_CLIENT_ID,
-	googleClientSecret: process.env.GOOGLE_SECRET,
-	sendGridKey: process.env.SENDGRID_KEY,
-	redirectDomain: process.env.CURR_DOMAIN,
+	host: "d6vm43116451544.db.43116451.615.hostedresource.net",
+	database: "d6vm43116451544",
+	username: "d6vm43116451544",
+	password: "pTbd@c61Z",
+	secretOrKey: "kldjki6778sh7MU6qE",
+	googleClientID:
+		"1047124688384-9vc66vvvnio35fvi8anqfmqkvecil7bp.apps.googleusercontent.com",
+	googleClientSecret: "atoW5RQdA33qipyktliceXzp",
+	sendGridKey:
+		"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",
+	redirectDomain: "http://localhost:3000",
+	confirmAccountKey: "mPaU4WKR77MU6qE",
 };
+
+// module.exports = {
+// 	host: process.env.DB_HOST,
+// 	database: process.env.DB_DATABASE,
+// 	username: process.env.DB_USERNAME,
+// 	password: process.env.DB_PASSWORD,
+// 	secretOrKey: process.env.DB_SecretOrKey,
+// 	googleClientID: process.env.GOOGLE_CLIENT_ID,
+// 	googleClientSecret: process.env.GOOGLE_SECRET,
+// 	sendGridKey: process.env.SENDGRID_KEY,
+// 	redirectDomain: process.env.CURR_DOMAIN,
+// };
